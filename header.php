99a31b4151bb217056e1819bc32051d80bc4329d
jdmMetz <joshmetzger6@outlook.com>
2021-03-30 15:53:57 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <div class="menuBar">
+        <button class="tablinks" onclick="window.location.href='employees.php';">Employees</button>
+        <button class="tablinks" onclick="window.location.href='patients.php';">Patients</button>
+        <button class="tablinks" onclick="window.location.href='appointments.php';">Appointments</button>
+        <button class="tablinks" onclick="window.location.href='occupied_rooms.php';">Rooms</button>
+        <form class="logout_button" method="POST">
+            <button type="submit" name="logout" class="logout_button">Logout</button>
+        </form>
+        <p class="login_as">Logged in as: <?php echo '       ';
+                                            print($_SESSION['user']); ?></p>
+    </div>
