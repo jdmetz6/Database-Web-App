261f4f255ecff29313ada6ff538aa48ec8561663
jdmMetz <joshmetzger6@outlook.com>
2021-04-08 01:09:33 +0700

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -1,11 +1,43 @@
 <?php include 'header.php'; ?>
 <h1 class="title">Appointments</h1>
 <?php
-include 'functions.php';
 $conn = db_connect($_SESSION['vali']);
-default_appointment_result($conn);
-logout_button();
-?>
-</body>
+$sql = "select patient.pid, patient.lname, patient.fname, appointment.date, appointment.time, employee.job_title, employee.lname, employee.fname 
+    from patient 
+    join appointment on appointment.pid = patient.pid 
+    join employee on employee.empid = appointment.empid;";
+$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
-</html>
+$field_names = [
+    'Patient ID',
+    'Patient Last Name',
+    'Patient First Name',
+    'Date',
+    'Time',
+    'Job Title',
+    'Employee Last Name',
+    'Employee First Name'
+];
+
+echo '<table class=tabl>';
+echo '<tr class=column>';
+// Print Column Names
+foreach ($field_names as $value) {
+    echo '<td>' . $value . '</td>';
+}
+echo '</tr>';
+
+// Print Data
+$fieldNum = mysqli_num_fields($result);
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr class=row>";
+    for ($x = 0; $x < $fieldNum; $x++) {
+        echo "<td>" . $row[$x] . "</td>";
+    }
+    echo "</br>";
+    echo "</tr>";
+}
+echo '</table>';
+$result->free_result();
+?>
+<?php include 'footer.php'; ?>
