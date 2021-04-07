33c9c92294855a26553653f52c273ddc9c8f2a1c
jdmMetz <joshmetzger6@outlook.com>
2021-04-08 01:09:49 +0700

    Update patients.php

--- patients.php
+++ patients.php
@@ -1,12 +1,44 @@
 <?php include 'header.php'; ?>
 <h1 class="title">Patient Information</h1>
 <?php
-include 'functions.php';
 $conn = db_connect($_SESSION['vali']);
-default_patient_result($conn);
-logout_button();
+$sql = 'select * from patient;';
+$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+$field_names = [
+    'ID',
+    'First Name',
+    'Last Name',
+    'Birthday',
+    'Gender',
+    'Address',
+    'Phone',
+    'Medications'
+];
+
+echo '<table class=tabl>';
+echo '<tr class=column>';
+// Print Column Names
+foreach ($field_names as $value) {
+    echo '<td>' . $value . '</td>';
+}
+echo '</tr>';
+
+// Print Data
+$fieldNum = mysqli_num_fields($result);
+while ($row = mysqli_fetch_array($result)) {
+    $i = $row[0];
+    echo "<tr class=row>";
+    for ($x = 0; $x < $fieldNum; $x++) {
+        echo "<td>" . $row[$x] . "</td>";
+    }
+    echo '<td><form class="med" method="post" action="medication.php">';
+    echo '<button class="med_button" type="submit" value="' . $i . '" name="pid">Check</button>';
+    echo '</form></td>';
+    echo '</br>';
+    echo '</tr>';
+}
+echo '</table>';
+$result->free_result();
 ?>
-
-</body>
-
-</html>
+<?php include 'footer.php'; ?>
