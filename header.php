93785bbc8e114a2071551ca61e3495c99d40b501
jdmMetz <joshmetzger6@outlook.com>
2021-04-08 12:11:01 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php session_start(); 
+<?php session_start();
 include 'functions.php';
 logout_button();
 ?>
@@ -17,10 +17,9 @@ logout_button();
         <button class="tablinks" onclick="window.location.href='appointments.php';">Appointments</button>
         <button class="tablinks" onclick="window.location.href='occupied_rooms.php';">Rooms</button>
         <form class="logout_box" method="POST">
-            <button  class="logout_button" type="submit" name="logout">Logout</button>
+            <button class="logout_button" type="submit" name="logout">Logout</button>
         </form>
         <p class="login_as">Logged in as: <?php echo '       ';
                                             print($_SESSION['user']); ?></p>
         <a class="help" href="https://github.com/jdmetz6/Database-Web-App/wiki" target="_blank"> Help</a>
     </div>
-    
