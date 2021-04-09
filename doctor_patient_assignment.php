07c0690311a06c6684c4760e10d386ce9ff2783f
jdmMetz <joshmetzger6@outlook.com>
2021-04-09 20:54:45 +0700

    Update doctor_patient_assignment.php

--- doctor_patient_assignment.php
+++ doctor_patient_assignment.php
@@ -16,26 +16,28 @@ $field_names = [
     'Time'
 ];
 if (mysqli_num_rows($result) > 0) {
-    echo '<table class=tabl>';
-    echo '<tr class=column>';
-    // Print Column Names
-    foreach ($field_names as $value) {
-        echo '<td>' . $value . '</td>';
-    }
-    echo '</tr>';
-
-    // Print Data
-    $fieldNum = mysqli_num_fields($result);
-    while ($row = mysqli_fetch_array($result)) {
-        $i = $row[0];
-        echo "<tr class=row>";
-        for ($x = 0; $x < $fieldNum; $x++) {
-            echo "<td>" . $row[$x] . "</td>";
+    echo '<div class="tabl_box">';
+        echo '<table class=tabl>';
+        echo '<tr class=column>';
+        // Print Column Names
+        foreach ($field_names as $value) {
+            echo '<td>' . $value . '</td>';
         }
-        echo "</br>";
-        echo "</tr>";
-    }
-    echo '</table>';
+        echo '</tr>';
+
+        // Print Data
+        $fieldNum = mysqli_num_fields($result);
+        while ($row = mysqli_fetch_array($result)) {
+            $i = $row[0];
+            echo "<tr class=row>";
+            for ($x = 0; $x < $fieldNum; $x++) {
+                echo "<td>" . $row[$x] . "</td>";
+            }
+            echo "</br>";
+            echo "</tr>";
+        }
+        echo '</table>';
+    echo '</div>';
     $result->free_result();
 } else {
     echo "<h2 class='no_result'>No Patients</h2>";
