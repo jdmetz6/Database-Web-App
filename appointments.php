7d1636ad00faeb91a87a689f744b1f83f7f3219d
jdmMetz <joshmetzger6@outlook.com>
2021-04-10 23:20:08 +0700

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -1,5 +1,8 @@
 <?php include 'header.php'; ?>
-<h1 class="title">Appointments</h1>
+<h1 id="tab_title" class="title">Appointments</h1>
+<script>
+    title()
+</script>
 <?php
 $conn = db_connect($_SESSION['vali']);
 $sql = "select patient.pid, patient.lname, patient.fname, appointment.date, appointment.time, employee.job_title, employee.lname, employee.fname 
@@ -18,28 +21,6 @@ $field_names = [
     'Employee Last Name',
     'Employee First Name'
 ];
-
-echo '<div class="tabl_box">';
-    echo '<table class=tabl>';
-    echo '<tr class=column>';
-    // Print Column Names
-    foreach ($field_names as $value) {
-        echo '<td>' . $value . '</td>';
-    }
-    echo '</tr>';
-
-    // Print Data
-    $fieldNum = mysqli_num_fields($result);
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<tr class=row>";
-        for ($x = 0; $x < $fieldNum; $x++) {
-            echo "<td>" . $row[$x] . "</td>";
-        }
-        echo "</br>";
-        echo "</tr>";
-    }
-    echo '</table>';
-echo '</div>';
-$result->free_result();
+print_results($field_names, $result, $conn);
 ?>
 <?php include 'footer.php'; ?>
