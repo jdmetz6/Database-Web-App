1177f4424e1360ac482044c5f97a008eb6ce4d90
jdmMetz <joshmetzger6@outlook.com>
2021-04-10 15:25:48 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
 </body>
+<div class="bottom_foot">
+    <div class="content_box">
+        <div class="footer_message_box">
+            <a href="https://github.com/jdmetz6/Database-Web-App" target="_blank">
+                <h4>
+                    <p class="footer_message">Hospital Database App. Find the code on Github.</p>
+                </h4>
+            </a>
+        </div>
+        <div class="github_logo_box">
+            <a href="https://github.com/jdmetz6/Database-Web-App" target="_blank"><img class="github_logo" src="images/github-logo.png" alt="github"></a>
+        </div>
+    </div>
+</div>
 
 </html>
