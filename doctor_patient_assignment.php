40b28f6f0ba7f4e3c2f03f08d2f5303669032636
jdmMetz <joshmetzger6@outlook.com>
2021-04-10 23:20:33 +0700

    Update doctor_patient_assignment.php

--- doctor_patient_assignment.php
+++ doctor_patient_assignment.php
@@ -1,5 +1,8 @@
 <?php include "header.php"; ?>
-<h1 class="title">Assigned Patients</h1>
+<h1 id="tab_title" class="title">Assigned Patients</h1>
+<script>
+    title()
+</script>
 <?php
 $conn = db_connect($_SESSION['vali']);
 $sql = "select patient.pid, patient.fname, patient.lname, appointment.date, appointment.time  
@@ -16,29 +19,7 @@ $field_names = [
     'Time'
 ];
 if (mysqli_num_rows($result) > 0) {
-    echo '<div class="tabl_box">';
-        echo '<table class=tabl>';
-        echo '<tr class=column>';
-        // Print Column Names
-        foreach ($field_names as $value) {
-            echo '<td>' . $value . '</td>';
-        }
-        echo '</tr>';
-
-        // Print Data
-        $fieldNum = mysqli_num_fields($result);
-        while ($row = mysqli_fetch_array($result)) {
-            $i = $row[0];
-            echo "<tr class=row>";
-            for ($x = 0; $x < $fieldNum; $x++) {
-                echo "<td>" . $row[$x] . "</td>";
-            }
-            echo "</br>";
-            echo "</tr>";
-        }
-        echo '</table>';
-    echo '</div>';
-    $result->free_result();
+    print_results($field_names, $result, $conn);
 } else {
     echo "<h2 class='no_result'>No Patients</h2>";
 }
