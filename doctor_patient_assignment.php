7b603ce7e908e15c206641099968adf0c7b7874c
jdmMetz <joshmetzger6@outlook.com>
2021-04-11 13:17:26 +0700

    Update doctor_patient_assignment.php

--- doctor_patient_assignment.php
+++ doctor_patient_assignment.php
@@ -1,16 +1,21 @@
 <?php include "header.php"; ?>
+
 <h1 id="tab_title" class="title">Assigned Patients</h1>
+
+<!-- Function call to change browser tab title. Definition is located in header.php file. -->
 <script>
     title()
 </script>
+
 <?php
+// Creating sql query.
 $conn = db_connect($_SESSION['validation']);
 $sql = "select patient.pid, patient.fname, patient.lname, appointment.date, appointment.time  
     from patient, appointment
     where  patient.pid=appointment.pid and appointment.empid = '" . $_POST['empid'] . "' ";
-
-
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+// Creating columns names for sql query results.
 $field_names = [
     'Patient ID',
     'Fist Name',
@@ -18,9 +23,12 @@ $field_names = [
     'Date',
     'Time'
 ];
+
+// Notify user if there are no results from the query.
 if (mysqli_num_rows($result) > 0) {
     print_results($field_names, $result, $conn);
 } else {
     echo "<h2 class='no_result'>No Patients</h2>";
 }
-include "footer.php";
+?>
+<?php include "footer.php"; ?>
