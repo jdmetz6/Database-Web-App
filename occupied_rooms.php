674bc7347ad28e1b49fea815634504466a77881a
jdmMetz <joshmetzger6@outlook.com>
2021-04-11 13:17:12 +0700

    Update occupied_rooms.php

--- occupied_rooms.php
+++ occupied_rooms.php
@@ -1,9 +1,14 @@
 <?php include 'header.php'; ?>
+
 <h1 id="tab_title" class="title">Rooms</h1>
+
+<!-- Function call to change browser tab title. Definition is located in header.php file. -->
 <script>
     title()
 </script>
+
 <?php
+// Creating sql query.
 $conn = db_connect($_SESSION['validation']);
 $sql = 'select patient.pid, patient.fname, patient.lname, room.room_type, room.room_number 
         from patient 
@@ -11,6 +16,7 @@ $sql = 'select patient.pid, patient.fname, patient.lname, room.room_type, room.r
         join room ON assign_room.room_number = room.room_number;';
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
+// Creating column names for sql query results.
 $field_names = [
     'Patient ID',
     'Patient First Name',
