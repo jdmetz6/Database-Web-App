8b45a9d7b26d4b5c5963010b8f6c4c3d2a580706
jdmMetz <joshmetzger6@outlook.com>
2021-04-11 13:17:23 +0700

    Update employees.php

--- employees.php
+++ employees.php
@@ -1,8 +1,13 @@
 <?php include 'header.php'; ?>
+
 <h1 id="tab_title" class="title">Employee Info.</h1>
+
+<!-- Function call to change browser tab title. Definition is located in header.php file. -->
 <script>
     title()
 </script>
+
+<!-- Add and delete employee buttons -->
 <div class="new_delete_box">
     <div class="new_emp_box">
         <form class="new_emp" method="POST">
@@ -15,15 +20,15 @@
         </form>
     </div>
 </div>
+
 <?php
-
+// Creating sql query.
 $conn = db_connect($_SESSION['validation']);
-new_emp_form($conn);
-delete_emp_form($conn);
-
 $sql = 'select * 
         from employee;';
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+// Creating column names for sql query results.
 $field_names = [
     'ID',
     'First Name',
@@ -37,6 +42,9 @@ $field_names = [
     'Hire Date',
     'Patients'
 ];
+
+new_emp_form($conn);
+delete_emp_form($conn);
 print_results($field_names, $result, $conn);
 ?>
 <?php include 'footer.php'; ?>
