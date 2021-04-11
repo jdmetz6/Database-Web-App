224b5f2501072dc30b93d444c9f0fdd8bf85b95e
jdmMetz <joshmetzger6@outlook.com>
2021-04-11 13:17:05 +0700

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -1,9 +1,14 @@
 <?php include 'header.php'; ?>
+
 <h1 id="tab_title" class="title">Appointments</h1>
+
+<!-- Function call to change browser tab title. Definition is located in header.php file. -->
 <script>
     title()
 </script>
+
 <?php
+// Creating sql query.
 $conn = db_connect($_SESSION['validation']);
 $sql = "select patient.pid, patient.lname, patient.fname, appointment.date, appointment.time, employee.job_title, employee.lname, employee.fname 
         from patient 
@@ -11,6 +16,7 @@ $sql = "select patient.pid, patient.lname, patient.fname, appointment.date, appo
         join employee on employee.empid = appointment.empid;";
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
+// Creating column names for sql query results.
 $field_names = [
     'Patient ID',
     'Patient Last Name',
@@ -21,6 +27,7 @@ $field_names = [
     'Employee Last Name',
     'Employee First Name'
 ];
+
 print_results($field_names, $result, $conn);
 ?>
 <?php include 'footer.php'; ?>
