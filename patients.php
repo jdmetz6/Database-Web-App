b9bf4dcc64a7d082d526c40e22485edde704059f
jdmMetz <joshmetzger6@outlook.com>
2021-04-11 13:17:10 +0700

    Update patients.php

--- patients.php
+++ patients.php
@@ -1,14 +1,20 @@
 <?php include 'header.php'; ?>
+
 <h1 id="tab_title" class="title">Patient Info.</h1>
+
+<!-- Function call to change browser tab title. Definition is located in header.php file. -->
 <script>
     title()
 </script>
+
 <?php
+// Creating sql query.
 $conn = db_connect($_SESSION['validation']);
 $sql = 'select * 
         from patient;';
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
+// Creating column names for sql query results.
 $field_names = [
     'ID',
     'First Name',
@@ -19,6 +25,7 @@ $field_names = [
     'Phone',
     'Prescriptions'
 ];
+
 print_results($field_names, $result, $conn);
 ?>
 <?php include 'footer.php'; ?>
